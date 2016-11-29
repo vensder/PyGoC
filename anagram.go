ec62e0c9e6d6d9c38b56711026eea83c5a33aad2
vensder <vensder@gmail.com>
2016-11-29 13:09:26 +0300

    anagram.go

--- anagram.go
+++ anagram.go
@@ -9,10 +9,12 @@ import (
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	СЛОВА := []string{"питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник"}
+    fmt.Printf("%T, %v\n", СЛОВА, СЛОВА)
 
 	слово := СЛОВА[rand.Intn(len(СЛОВА))]
+    fmt.Printf("%T, %v\n", слово, слово)
 	//	верное := слово
-	анаграмма := []rune{}
+	анаграмма := []string{}
 
 	println(слово)
 	word := []rune{}
@@ -26,15 +28,19 @@ func main() {
 		println()
 	}
 
-	println(len(word))
+    fmt.Printf("Word %T\n", string(word))
 
+    println("Len of Word: ", len(word))
+/*
 	for len(word) >= 0 {
 		позиция := rand.Intn(len(word))
 		анаграмма = append(анаграмма, word(позиция))
 		word = append(word[:позиция],  word[(позиция+1):])
 	}
+*/
+    // начало игры
 
-	// начало игры
+
 	fmt.Println(`Добро пожаловать в игру "Анаграммы"!
     Надо переставить буквы так, чтобы получилось осмысленное слово.
     (Для выхода нажмите Enter, не вводя своей версии.)
