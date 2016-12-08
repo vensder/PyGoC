729d3f3c1b674662eee72a55444c0775a85c8e61
vensder <vensder@gmail.com>
2016-12-09 02:05:16 +0300

    kludged go version

--- anagram.go
+++ anagram.go
@@ -9,43 +9,32 @@ import (
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	СЛОВА := []string{"питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник"}
-    fmt.Printf("%T, %v\n", СЛОВА, СЛОВА)
 
 	слово := СЛОВА[rand.Intn(len(СЛОВА))]
-    fmt.Printf("%T, %v\n", слово, слово)
-	//	верное := слово
-	анаграмма := []string{}
+	//верное := слово
+	анаграмма := []rune{}
 
-	println(слово)
+	println("Это временная подсказка для тестирования, \nслово:", слово)
 	word := []rune{}
 	for _, letter := range слово {
 		word = append(word, letter)
 	}
-	fmt.Printf("%c\n", word)
 
-	for i, letter := range word {
-		fmt.Printf("%d: %c, %c\n", i, letter, (word[i]))
-		println()
-	}
-
-    fmt.Printf("Word %T\n", string(word))
-
-    println("Len of Word: ", len(word))
-/*
-	for len(word) >= 0 {
+	fmt.Printf("%d\n", len(word))
+	for len(word) > 0 {
 		позиция := rand.Intn(len(word))
-		анаграмма = append(анаграмма, word(позиция))
-		word = append(word[:позиция],  word[(позиция+1):])
+		анаграмма = append(анаграмма[:], word[позиция:(позиция+1)]...)
+		word = append(word[:позиция], word[(позиция+1):]...)
+		fmt.Printf("%d\n", len(word))
 	}
-*/
-    // начало игры
-
 
 	fmt.Println(`Добро пожаловать в игру "Анаграммы"!
     Надо переставить буквы так, чтобы получилось осмысленное слово.
     (Для выхода нажмите Enter, не вводя своей версии.)
     `)
-	fmt.Println("Вот анаграмма: ", анаграмма)
+
+	fmt.Printf("Вот агаграмма: %c\n", анаграмма)
+
 	//отгадка = input("\nПопробуйте отгадать исходное слово: ")
 	//while отгадка != верное and отгадка != "":
 	//    print("К сожалению, вы неправы.")
