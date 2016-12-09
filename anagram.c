c23c218cd6011796c3e012b05dea293e598b566e
vensder <vensder@gmail.com>
2016-12-10 01:25:23 +0300

    anagram.c

---
+++ anagram.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+char *words[] = {"питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник"};
+char *word;
+
+int main () {
+
+	word = words[4];
+	printf("Выбранное слово: %s\n", word);
+	printf("Длина слова: %lu\n", strlen(word));
+
+	printf("Берем буквы через пары указателей:\n");
+	char *p1; char *p2;
+	    int i = 0;
+	    while(i < strlen(word)) {
+	        p1 = &word[i];
+	        p2 = &word[i+1];
+	        printf("*p1*p2 = %c%c(%d) \n", *p1,*p2, i);
+	        i++; i++;
+	    }
+
+	 printf("Выводим слово через putchar: \n");  
+	 int index = 0;
+	 while (word[index] != '\0') {
+	 	putchar(word[index]);
+	 	putchar(word[index+1]);
+	 	printf(" i: %d\n", index);
+	 	index++; index++;
+	 }
+
+	printf("\nРазмер элемента слова: %lu\n", sizeof(&word[0]));
+	printf("Первая буква в числовом представлении: %d\n", word[0]);
+}
