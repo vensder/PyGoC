3dbbe1fd4add880040bbc78f11b9002d6e8620f8
vensder <vensder@gmail.com>
2016-12-11 02:32:40 +0300

    anagram.c finished

--- anagram.c
+++ anagram.c
@@ -3,54 +3,66 @@
 #include <stdlib.h>
 #include <time.h>
 
-char *words[] = {"питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник"};
-char *word; char *right_word; char anagram[60];
+char *words[] = {"питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник", "синхрофазатрон"};
+char *word = {0}; char *right_word = {0}; char anagram[60] = {0};
+char guess[30];
 time_t t;
 int rand_number;
 
 int main () {
 
-	srand((unsigned) time(&t)); // бросаем семя seed в генератор случайных чисел 
-	rand_number = rand() % 6; // генератор случайных чисел от 0 до 5 для выбора слова
-	if (rand_number > 5) { // на всякий случай, чтобы успокоить параною выхода за пределы массива
-		rand_number = 5;
-	}
-	printf("%d\n", rand_number);
-	word = words[rand_number]; // выбираем случайное слово из списка слов
-	right_word = word;
+    printf("\tДобро пожаловать в игру \"Анаграммы\"!\n");
+    printf("Надо переставить буквы так, чтобы получилось осмысленное слово.\n");
+    printf("(Для выхода просто нажимайте Enter, не вводя своей версии.)\n");
 
-	rand_number = rand() % strlen(word); // пара байт на русскую буквую, работаем с парами.
-	printf("rand_number: %d\n", rand_number);
-	// извлекаем эту случайную букву (пару байт - четный:нечетный) из слова, 
-	// помещаем в другое (anagram)
+	srand((unsigned) time(&t)); // бросаем семя seed в генератор случайных чисел (ГСЧ)
+	rand_number = rand() % sizeof(words)/sizeof(words[0]); // ГСЧ от 0 до числа элементов минус один
 
-	char buf[10];
-	memset(buf,0,sizeof(buf));
-	strncpy(buf, &word[0], 2); // как раз два байта в буфер.
-	strcat(anagram, buf); // конкатенируем случайную букву к анаграмме
-	printf("anagram: %s\n", anagram);
+	word = strdup(words[rand_number]); // выбираем случайное слово из списка слов
+	right_word = strdup(word);
 
-
+	while (strlen(word) > 0) {
+		// 9 = 59 % 10
+		rand_number = rand() % strlen(word); // случайное число от нуля до последнего байта -1
+		if (rand_number % 2 != 0) {	// используем только четные числа,т.к. работаем с парами.
+			if (rand_number == strlen(word)-1)
+				rand_number--;  // 10
+			else
+				rand_number++;
+		}
+		anagram[strlen(anagram)] = word[rand_number];
+		anagram[strlen(anagram)] = word[rand_number+1];
+		int i;
+		for (i = rand_number; i < strlen(word)-2 && word[i] != '\0'; i++) {
+			word[i] = word[i + 2];
+		}
+		for (; i < strlen(word); i++) {
+			word[i] = '\0';
+		}
 	
-	int i = 0;
-	while (i < strlen(word)) {
-	//memset(buf,0,sizeof(buf));
-	strncpy(buf, &word[i], 2);
-	printf("Temp buf: %s\n", buf);
-	i++; i++;
-}
-	printf("Выбранное слово: %s\n", word);
-	printf("Длина слова: %lu\n", strlen(word));
+	}
 
-	 printf("Выводим слово через putchar: \n");  
-	 int index = 0;
-	 while (index < strlen(word)) {
-	 	putchar(word[index]);
-	 	putchar(word[index+1]);
-	 	printf(" i: %d\n", index);
-	 	index++; index++;
-	 }
+	printf("У нас получилась такая анаграмма: %s\n", anagram);
+	printf("Попробуйте разгадать ее и ввести правильное слово:");
+	fgets(guess, 30, stdin);
+	strtok(guess, "\n");
+	if (strcmp(guess, "\n") != 0) {
+		printf("Итак, Вы полагаете, что это %s\n", guess);
+		printf("Нажмите Enter, чтобы поскорее узнать, угадали Вы или нет!");
+	} else {
+		printf("Если ничего не приходит на ум, то можете просто нажать Enter!\n");
+	}
+	getchar();
+	if ((strcmp(right_word, guess)) == 0) {
+		printf("Поздравляем! Вы очень проницательны! Вы угадали!");
+	} else {
+		printf("К сожалению, Вы не угадали. Но может повезет в другой раз!\n");
+		printf("Нажмите Enter, чтобы подсмотреть правильный ответ: \n");
+	    getchar();	
+		printf("Правильное слово: '%s'\n", right_word);
+	}
 
-	printf("\nРазмер элемента слова: %lu\n", sizeof(&word[0]));
-	printf("Первая буква в числовом представлении: %d\n", word[0]);
+    printf("Нажмите Enter для выхода: \n");
+    getchar();
+
 }
