18e37c793ea990d69bf3032e66feddcf25ffabb5
vensder <vensder@gmail.com>
2016-12-17 14:13:17 +0300

    tryed to work with uniconde strings directly - it\'s bullshit

--- anagram_strings.go
+++ anagram_strings.go
@@ -12,18 +12,17 @@ func main() {
 
 	слово := СЛОВА[rand.Intn(len(СЛОВА))]
 	верное := слово
-	анаграмма := []rune{}
+	var анаграмма, word string
 
-	word := []rune{}
 	for _, letter := range слово {
-		word = append(word, letter)
+		word = word + string(letter)
 	}
 
 	fmt.Printf("%d\n", len(word))
 	for len(word) > 0 {
 		позиция := rand.Intn(len(word))
-		анаграмма = append(анаграмма[:], word[позиция:(позиция+1)]...)
-		word = append(word[:позиция], word[(позиция+1):]...)
+		анаграмма = анаграмма + word[позиция:(позиция+1)]
+		word = word[:позиция] +  word[(позиция+1):]
 	}
 
 	fmt.Println(`Добро пожаловать в игру "Анаграммы"!
