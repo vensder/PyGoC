f6bc1765a7157e6f3794b54c9dfed0d6159848f1
Dmitry Makarov <vensder@gmail.com>
2017-01-02 20:02:45 +0300

    some improvements

--- fibonacci_recursion.go
+++ fibonacci_recursion.go
@@ -19,7 +19,6 @@ func fibonacci(n int) int {
 }
 
 func main() {
-	fmt.Println(len(os.Args))
 	if len(os.Args) < 2 {
 		fmt.Println(`
 			Введите порядковый номер числа
@@ -32,13 +31,15 @@ func main() {
 			fmt.Println(err)
 			os.Exit(2)
 		}
-		fmt.Println(n)
+		t0 = time.Now()
 		for i := 0; i <= n; i++ {
 			fib_arr = append(fib_arr, fibonacci(i))
 		}
+		t1 = time.Now()
 		fmt.Printf("%v-й член последовательности Фибоначчи - это: %v\n", n, fib_arr[n])
 		fmt.Printf("Вся последовальеность до %v-го члена:\n", n)
 		fmt.Println(fib_arr)
+		fmt.Printf("Время вычисления составило %v.\n", t1.Sub(t0))
 	}
 
 }
