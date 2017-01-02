12d8203f9b6be336f1cce2ec606cbdd066fb6a0e
vensder <vensder@gmail.com>
2017-01-03 00:44:09 +0300

    added input from cli into fibonacci_recursion.go

--- fibonacci_recursion.go
+++ fibonacci_recursion.go
@@ -9,6 +9,7 @@ import (
 
 var t0, t1 time.Time
 var fib_arr = []int{}
+var fib_number string
 
 func fibonacci(n int) int {
 	if n < 2 {
@@ -20,26 +21,35 @@ func fibonacci(n int) int {
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println(`
-			Введите порядковый номер числа
-			из последовательности Фибоначчи
-			как аргумент запуска приложения.
-			`)
+		fmt.Print(`
+        Вы можете передать порядковый
+        номер числа Фибоначчи как аргумент
+        при запуске этой программы.
+
+        Вычисление чисел Фибоначчи
+        рекурсивным методом более 30...40 может
+        занять много времени.
+
+        Введите порядковый номер числа
+        из последовательности Фибоначчи:`)
+		fmt.Scanln(&fib_number)
 	} else {
-		n, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(2)
-		}
+		fib_number = (os.Args[1])
+	}
+
+	n, err := strconv.Atoi(fib_number)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	} else {
 		t0 = time.Now()
 		for i := 0; i <= n; i++ {
 			fib_arr = append(fib_arr, fibonacci(i))
 		}
 		t1 = time.Now()
 		fmt.Printf("%v-й член последовательности Фибоначчи - это: %v\n", n, fib_arr[n])
-		fmt.Printf("Вся последовальеность до %v-го члена:\n", n)
+		fmt.Printf("Вся последовательность до %v-го члена:\n", n)
 		fmt.Println(fib_arr)
 		fmt.Printf("Время вычисления составило %v.\n", t1.Sub(t0))
 	}
-
 }
